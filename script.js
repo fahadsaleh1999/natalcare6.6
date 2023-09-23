1b3bf6e20c7ecc5a36b749f25b947c24bdd1b0d1
fahadsaleh1999 <143285274+fahadsaleh1999@users.noreply.github.com>
2023-09-23 23:07:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// let menubar =  document.querySelector('#menubar');
+// let mynav =  document.querySelector('.navbar');
+
+
+// menubar.onclick = () => {
+//     menubar.classList.toggle('fa-times')
+//     mynav.classList.toggle('active')
+// }
+let menubar = document.querySelector('#menubar');
+let mynav = document.querySelector('.navbar');
+
+menubar.onclick = () => {
+  menubar.classList.toggle('fa-times');
+  mynav.classList.toggle('active');
+}
+
+
+let navLinks = document.querySelectorAll('.navbar a');
+
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    menubar.classList.toggle('fa-times');
+    mynav.classList.toggle('active');
+  });
+});
